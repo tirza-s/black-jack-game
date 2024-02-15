838b743b05445f9b41f2b42992996775f56a33a1
Tirza Samosir <tirzasamosir@MacBook-Air-de-Tirza.local>
2024-02-15 13:13:12 +0100

    add the querySelector

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 let firstCard = 11
-let secondCard = 15
+let secondCard = 8
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
+
 let message = " "
 
+let message_El = document.getElementById("message_el")
+let sum_El = document.querySelector("#sum_el")
+
+console.log(sum_El);
+
 function startGame() {
+    sum_El.textContent = "Sums : " + sum
     if (sum <= 20) {
         message = "Do you want to draw another card ? 😀"
     } else if (sum === 21) {
@@ -15,8 +22,9 @@ function startGame() {
         message = "Oppss, you are out of the game! 😭"
         isAlive = (false)
     }
-    console.log(message);
-}
+    // to display the message
+    message_El.textContent = message
 
+}
 
 
